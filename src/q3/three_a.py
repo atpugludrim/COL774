Fixed the variance bug

src/q3/three_a.py
@@ -37,7 +37,7 @@ class Scaler:
         for x in data:
             mean += x/n
             var += np.multiply(x,x)/(n-1)
-        var = var - mean * (n-1)/n
+        var = var - mean * mean * n/(n-1)
         this.mu = mean
         this.sig_sq = var
 