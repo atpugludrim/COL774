0f1ada07798ff35e0a851873e7f3dc3643c5750e
atpugludrim <mridulgupta82@gmail.com>
2021-10-25 21:15:12 +0530

    q1d

---
+++ src/q1/q1d.py
@@ -0,0 +1,168 @@
+import sys
+import time
+import copy
+import pickle
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier as RFC
+
+class DummyFile:
+    def write(this, *args, **kwargs):pass
+    def flush(this, *args, **kwargs):pass
+
+def timeitdec(func):
+    def f(*args):
+        s = time.time()
+        ret = func(*args)
+        e = time.time()
+        print("Aisa hai beta ki tumhe \"{}\" chalane mei {} seconds lage hai".format(func.__name__,(e-s)))
+        return ret
+    return f
+
+def silentdec(silent=True):
+    def f(func):
+        def f1(*args):
+            std = sys.stdout
+            ste = sys.stderr
+            print("Silencing \"{}\"".format(func.__name__),file=sys.stderr)
+            sys.stdout = DummyFile()
+            sys.stderr = DummyFile()
+            ret = func(*args)
+            sys.stdout = std
+            sys.stderr = ste
+            return ret
+        if silent:
+            return f1
+        else:
+            return func
+    return f
+
+class timeit:
+    def __enter__(this):
+        this.s = time.time()
+    def __exit__(this,a,b,c):
+        print("Runtime: {:.3f}s".format(time.time()-this.s))
+
+def increase_like_clockwork(ind, max_ind, idx):
+    if idx >= len(ind):
+        return False
+    ind[idx] += 1
+    ret=True
+    if ind[idx] >= max_ind[idx]:
+        ind[idx] = 0
+        ret=increase_like_clockwork(ind, max_ind, idx+1)
+    return ret
+
+@timeitdec
+@silentdec(not True)
+def main():
+    # 1) HAVE TO MAKE DUMMIES
+    df = pd.read_csv('/home/anupam/Desktop/backups/COL774/data/q1/bank_train.csv',delimiter=';')
+    xdf_train = df.iloc[:,:-1]
+    xdf_train_oneh = pd.get_dummies(xdf_train)
+    ts = len(df)#3000
+    ind = np.random.permutation(len(xdf_train_oneh))[:ts]
+    xdf_train_oh = xdf_train_oneh.iloc[ind]
+    ydf_train = df.iloc[ind,-1]
+    print("Training data loaded")
+
+    # 2)
+    df = pd.read_csv('/home/anupam/Desktop/backups/COL774/data/q1/bank_test.csv',delimiter=';')
+    xdf_test = df.iloc[:,:-1]
+    xdf_test_oh = pd.get_dummies(xdf_test)
+    missing_cols = set(xdf_train_oh.columns) - set(xdf_test_oh.columns)
+    for c in missing_cols:
+        xdf_test_oh[c] = 0
+    xdf_test_oh = xdf_test_oh[xdf_train_oh.columns]
+    ydf_test = df.iloc[:,-1]
+    print("Testing data loaded")
+
+    # 3)
+    df = pd.read_csv('/home/anupam/Desktop/backups/COL774/data/q1/bank_val.csv',delimiter=';')
+    xdf_val = df.iloc[:,:-1]
+    xdf_val_oh = pd.get_dummies(xdf_val)
+    missing_cols = set(xdf_train_oh.columns) - set(xdf_val_oh.columns)
+    for c in missing_cols:
+        xdf_val_oh[c] = 0
+    xdf_val_oh = xdf_val_oh[xdf_train_oh.columns]
+    ydf_val = df.iloc[:,-1]
+    print("Validation data loaded")
+
+    # make param_grid
+    optimum = {'n_estimators':450,'max_features':0.9,'min_samples_split':10}
+    param_grid = [{'n_estimators':[_ for _ in range(50,451,100)],
+        'max_features':np.arange(0.1,1,0.2),
+        'min_samples_split':np.arange(2,11,2)}]
+
+    # MAKE CLASSIFIER WITH VARIOUS PARAMETERS
+    clf = RFC()
+
+    params = np.array(["n_estimators","max_features","min_samples_split"])
+    for param in range(len(params)):
+        cur_param = params[param]
+        others = params[np.where(params!=params[param])]
+        # code here
+        print(f"training classifiers for {cur_param}")
+        val_acc  = []
+        test_acc = []
+        for o in others:
+            setattr(clf,o,optimum[o])
+        for idx in range(len(param_grid[0][cur_param])):
+            print(cur_param,"=",param_grid[0][cur_param][idx])
+            setattr(clf,cur_param,param_grid[0][cur_param][idx])
+            clf.fit(xdf_train_oh.values, ydf_train.values)
+            y_hat = clf.predict(xdf_val_oh.values)
+            y_tru = ydf_val.values
+            val_acc.append(np.sum(y_hat==y_tru)/y_tru.shape[0])
+            y_hat = clf.predict(xdf_test_oh.values)
+            y_tru = ydf_test.values
+            test_acc.append(np.sum(y_hat==y_tru)/y_tru.shape[0])
+        fig=plt.figure()
+        plt.plot(param_grid[0][cur_param],val_acc,'C0--',label='validation')
+        plt.plot(param_grid[0][cur_param],test_acc,'r--',label='test')
+        plt.xlabel("{} values".format(cur_param))
+        plt.ylabel("Accuracy")
+        plt.legend()
+        plt.savefig('sensitivity_{}'.format(cur_param))
+        plt.close(fig)
+
+    # ctr = 0
+    # while True:
+    #     # code here
+    #     ctr += 1
+    #     print(f"training classifier {ctr}")
+    #     for idx in range(len(ind)):
+    #         setattr(clf,list(param_grid[0].keys())[idx],param_grid[0][list(param_grid[0].keys())[idx]][ind[idx]])
+    #     clf.fit(xdf_train_oh.values, ydf_train.values)
+    #     if clf.oob_score_ > best_oob:
+    #         best_oob = clf.oob_score_
+    #         best_params['n_estimators'] = param_grid[0]['n_estimators'][ind[idx]]
+    #         best_params['max_features'] = param_grid[0]['max_features'][ind[idx]]
+    #         best_params['min_samples_split'] = param_grid[0]['min_samples_split'][ind[idx]]
+    #         best_model = copy.deepcopy(clf)
+    #     if not increase_like_clockwork(ind, max_ind, 0):
+    #         break
+
+    # with open("best_model_{}.pkl".format(ts),"wb") as f:
+    #     pickle.dump(best_model, f)
+    # with open("best_params_{}.pkl".format(ts),"wb") as f:
+    #     pickle.dump(best_params, f)
+
+    # y_hat_train = clf.predict(xdf_train_oh.values)
+    # y_hat_test = clf.predict(xdf_test_oh.values)
+    # y_hat_val = clf.predict(xdf_val_oh.values)
+    # 
+    # acc_train = np.sum(y_hat_train == ydf_train.values)/y_hat_train.shape[0]
+    # acc_test = np.sum(y_hat_test == ydf_test.values)/y_hat_test.shape[0]
+    # acc_val = np.sum(y_hat_val == ydf_val.values)/y_hat_val.shape[0]
+
+    # f = open("accuracies_rf_{}.txt".format(ts),"w")
+    # f.write("Train {}\n".format(acc_train))
+    # f.write("Test {}\n".format(acc_test))
+    # f.write("Val {}\n".format(acc_val))
+    # f.close()
+
+if __name__ == "__main__":
+    main()
